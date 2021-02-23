8ae22e1e640eb55d459206e77ae698a6c87c6391
Joel Mendivil <jmendivil171@west-mec.org>
2021-02-23 14:49:17 -0700

    Final changes

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,4 @@ router.get('/', function(req, res) {
   res.redirect('/catalog');
 });
 
-/* GET incident page. */
-router.get('/', function(req, res) {
-  res.redirect('/incident');
-});
-
 module.exports = router;
