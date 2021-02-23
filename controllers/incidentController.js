7b6cf396a17f6ef2b8a95d676881c68c318489c3
Meadow Green <meadowg.4543@gmail.com>
2021-02-23 14:49:27 -0700

    Auto stash before merge of "main" and "origin/main"

--- controllers/incidentController.js
+++
@@ -1,6 +0,0 @@
-var Incident = require('../models/incident');
-
-// Display list of all Incident .
-exports.index = function(req, res) {
-    res.render('index', { title: 'Records Management System', error: err, data: results});
-};
